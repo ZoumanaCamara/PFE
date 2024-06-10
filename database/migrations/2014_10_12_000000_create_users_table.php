8192d825c7a660b00719a631d24cb7107ed412c1
ZoumanaCamara <camarazou78@gmail.com>
2024-06-10 20:46:56 +0000

    update

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -23,32 +23,6 @@ return new class extends Migration
             $table->rememberToken();
             $table->timestamps();
         });
-
-        Schema::table('annoucements', function (Blueprint $table) {
-            $table->foreignIdFor(User::class)->constrained()->cascadeOnDelete(); 
-        });
-
-        Schema::table('advertisements', function (Blueprint $table) {
-            $table->foreignIdFor(User::class)->constrained()->cascadeOnDelete(); 
-        });
-
-        Schema::table('events', function (Blueprint $table) {
-            $table->foreignIdFor(User::class)->constrained()->cascadeOnDelete(); 
-        });
-
-        Schema::table('articles', function (Blueprint $table) {
-            $table->foreignIdFor(User::class)->constrained()->nullOnDelete(); 
-        });
-
-        Schema::table('purchases', function (Blueprint $table) {
-            $table->foreignIdFor(User::class)->constrained()->cascadeOnDelete(); 
-        });
-
-        Schema::table('carts', function (Blueprint $table) {
-            $table->foreignIdFor(User::class)->constrained()->cascadeOnDelete(); 
-        });
-
-
     }
 
     /**
@@ -58,32 +32,6 @@ return new class extends Migration
     {
 
         Schema::dropIfExists('users');
-
-
-        Schema::table('annoucements', function (Blueprint $table) {
-            $table->dropForeignIdFor(User::class); 
-        });
-
-        Schema::table('advertisements', function (Blueprint $table) {
-            $table->dropForeignIdFor(User::class); 
-        });
-
-        Schema::table('events', function (Blueprint $table) {
-            $table->dropForeignIdFor(User::class); 
-        });
-
-        Schema::table('articles', function (Blueprint $table) {
-            $table->dropForeignIdFor(User::class); 
-        });
-
-        Schema::table('purchases', function (Blueprint $table) {
-            $table->dropForeignIdFor(User::class); 
-        });
-
-        Schema::table('carts', function (Blueprint $table) {
-            $table->dropForeignIdFor(User::class); 
-        });
-
 
     }
 };
